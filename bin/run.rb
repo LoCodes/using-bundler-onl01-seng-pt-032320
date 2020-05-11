d10054457c9154682871d395caa178199cee38bc
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-11 06:02:32 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,8 @@
-In the example above, we're first requiring 'bundler/setup'. If we don't do this,
-our app won't know to use bundler to install our gems. Without that line, our Gemfile becomes pointless.
-
-Important: The two arguments that you are passing into the .require method must be passed in the correct order,
- shown above. The test you are trying to pass is testing for order. 
+# In the example above, we're first requiring 'bundler/setup'. If we don't do this,
+# our app won't know to use bundler to install our gems. Without that line, our Gemfile becomes pointless.
+#
+# Important: The two arguments that you are passing into the .require method must be passed in the correct order,
+#  shown above. The test you are trying to pass is testing for order.
 
 
  # should make the bundler gems available in bin/run.rb
