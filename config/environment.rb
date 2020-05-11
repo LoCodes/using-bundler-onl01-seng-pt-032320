af2803e62beb2492256d70d99ae2dc6c6c5412cd
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-11 05:52:05 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,12 @@
 # integration
 #    should require bundler/setup in the environment
 #    should require the default and development groups in the environment
+# 
+# groups
+# rm: cannot remove '.bundle/config': No such file or directory
+#   should contain the pry gem in the development group using a hash argument to the gem method (FAILED - 1)
+# rm: cannot remove '.bundle/config': No such file or directory
+#   should contain the rspec gem in the test group using block syntax
 
 
 require 'bundler/setup'
